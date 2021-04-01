7afae47bfe4acb352e8ab321cad1a844949b3af4
Maxence <maxgrymonprez@live.fr>
2021-04-01 18:16:25 +0100

    rename files

--- Start-psake.ps1
+++ Start-psake.ps1
@@ -1,2 +1,4 @@
+Import-Module psake
+
 $psakefile = Join-Path -Path $PSScriptRoot -ChildPath "psakefile.ps1"
 Invoke-psake -buildFile $psakefile -InformationAction Continue
--- psakefile.ps1
+++ psakefile.ps1
@@ -1,9 +1,9 @@
 Task default -depends Build, Test
 
 Task Build {
-    & "$PSScriptRoot\build.ps1"
+    & "$PSScriptRoot\Build-Module.ps1"
 }
 
 Task Test -depends Build {
-    & "$PSScriptRoot\Run-Tests.ps1"
+    & "$PSScriptRoot\Test-Module.ps1"
 }
