13797a4e25f96c97e18a726b57b0388b191d5283
Maxence <maxgrymonprez@live.fr>
2021-04-01 19:31:12 +0100

    Update Install-Dependences.ps1

--- Install-Dependences.ps1
+++ Install-Dependences.ps1
@@ -1,6 +1,5 @@
-Install-Module PSDepend
-Import-Module PSDepend
+Install-Module -Name PSDepend
+Import-Module -Name PSDepend
 
 $RequirementsFile = Join-Path -Path $PSScriptRoot -ChildPath requirements.psd1
-
 Invoke-PSDepend -Path $RequirementsFile
