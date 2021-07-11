4f0da9024cb26ccd4d07ab91fc7f4910cdad66c9
Maxence <maxgrymonprez@live.fr>
2021-07-11 01:49:01 +0100

    Bump version to 0.3.1

--- build.settings.ps1
+++ build.settings.ps1
@@ -1,7 +1,7 @@
 $ProjectRoot = $PSScriptRoot
 
 $ModuleName = "MyRemoteManager"
-$ModuleVersion = "0.3.0"
+$ModuleVersion = "0.3.1"
 
 $Source = Join-Path -Path $ProjectRoot -ChildPath "Source"
 $Tests = Join-Path -Path $ProjectRoot -ChildPath "Tests"
